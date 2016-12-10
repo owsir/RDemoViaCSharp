0889cc4e10cf5e7fbfd87f98f31fae4b3af383ef
david <12833318@qq.com>
2016-12-10 12:29:29 +0800

    Add files

---
+++ RDemoViaCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using RserveCli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDemoViaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverAddress = IPAddress.Parse("192.168.1.106");
+            var serverPort = 6311;
+
+            using (var s = new RConnection(serverAddress, serverPort, "david", "123456"))
+            {
+                s.VoidEval("library(forecast)");
+            }
+
+            }
+
+    }
+}
